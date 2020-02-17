be0fd87ec9364aa45b8521a5abe5673fa013404a
Somya Guglani <somyaguglani0897@gmail.com>
2020-02-17 16:03:07 +0530

    done with js part

--- JavaScript Drum Kit/script.js
+++ JavaScript Drum Kit/script.js
@@ -1,9 +1,18 @@
 window.addEventListener(`keydown`, function(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   //   console.log(audio);
-  if (!audio)
-    //handles keys that don't have values associated to them
-    return;
-  audio.currentTime() = 0; //rewinds time
+  //   console.log(key);
+  //for classes we use dots for others we don't
+  //for handling conditions when value is null
+  if (!audio) return;
+  audio.currentTime = 0; //rewinds time
   audio.play();
+  key.classList.add(`pressing`);
 });
+function removeTransition(e) {
+  if (e.propertyName !== `transform`) return;
+  this.classList.remove(`pressing`);
+}
+const keys = document.querySelectorAll(`.key`);
+keys.forEach(key => key.addEventListener(`transitionend`, removeTransition));
