b65358e4b43f2897d2221961f01fcf7d9ea2ba4e
Somya Guglani <somyaguglani0897@gmail.com>
2020-02-17 16:08:58 +0530

    all the code has been written in a clear matter

--- JavaScript Drum Kit/script.js
+++ JavaScript Drum Kit/script.js
@@ -1,18 +1,18 @@
-window.addEventListener(`keydown`, function(e) {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  //   console.log(audio);
-  //   console.log(key);
   //for classes we use dots for others we don't
   //for handling conditions when value is null
   if (!audio) return;
   audio.currentTime = 0; //rewinds time
   audio.play();
   key.classList.add(`pressing`);
-});
+}
 function removeTransition(e) {
   if (e.propertyName !== `transform`) return;
   this.classList.remove(`pressing`);
 }
 const keys = document.querySelectorAll(`.key`);
+//we call functions without using () like given below
 keys.forEach(key => key.addEventListener(`transitionend`, removeTransition));
+window.addEventListener(`keydown`, playSound);
