c4c78f68bdef08babbc1276c52f05661182434d1
Somya Guglani <somyaguglani0897@gmail.com>
2020-02-25 17:25:22 +0530

    working on js part

--- Etch-A-Sketch/etchasketch.js
+++ Etch-A-Sketch/etchasketch.js
@@ -1,1 +1,14 @@
-
+const canvas = document.querySelector(`.etch-a-sketch`);
+const btn = document.querySelector(`.btn`);
+const ctx = canvas.getContext(`2d`);
+// console.dir(canvas);
+const { width, height } = canvas; //easier to write
+console.log(width + ` ` + height);
+console.log(ctx);
+ctx.lineJoin = `round`;
+ctx.lineCap = `round`;
+ctx.lineWidth = 10;
+ctx.beginPath();
+ctx.moveTo(200, 200);
+ctx.lineTo(200, 200);
+ctx.stroke();
