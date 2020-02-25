e156de3318faa976c2d2c5718c504b2766bf3abd
Somya Guglani <somyaguglani0897@gmail.com>
2020-02-25 18:56:31 +0530

    almost done with js part

--- Etch-A-Sketch/etchasketch.js
+++ Etch-A-Sketch/etchasketch.js
@@ -1,14 +1,54 @@
 const canvas = document.querySelector(`.etch-a-sketch`);
 const btn = document.querySelector(`.btn`);
 const ctx = canvas.getContext(`2d`);
-// console.dir(canvas);
+const MOVE_AMOUNT = 10;
 const { width, height } = canvas; //easier to write
-console.log(width + ` ` + height);
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
+let hue = 0;
+ctx.strokeStyle = `hsl(${hue},100%,50%)`;
+
 console.log(ctx);
 ctx.lineJoin = `round`;
 ctx.lineCap = `round`;
-ctx.lineWidth = 10;
+ctx.lineWidth = MOVE_AMOUNT;
 ctx.beginPath();
-ctx.moveTo(200, 200);
-ctx.lineTo(200, 200);
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
 ctx.stroke();
+
+// function draw(options) { //we can pass options object
+
+// object de-structuring
+function draw({ key }) {
+  hue += 2;
+  ctx.strokeStyle = `hsl(${hue},100%,50%)`;
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  switch (key) {
+    case `ArrowUp`:
+      y -= MOVE_AMOUNT;
+      break;
+    case `ArrowDown`:
+      y += MOVE_AMOUNT;
+      break;
+    case `ArrowLeft`:
+      x -= MOVE_AMOUNT;
+      break;
+    case `ArrowRight`:
+      x += MOVE_AMOUNT;
+      break;
+    default:
+      break;
+  }
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
+
+function handleKey(e) {
+  if (e.key.includes(`Arrow`)) {
+    e.preventDefault();
+    draw({ key: e.key });
+  }
+}
+window.addEventListener(`keydown`, handleKey);
