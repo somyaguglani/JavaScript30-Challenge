06d12d9d5814b41208e6f92a8a76a879bf71f933
Somya Guglani <somyaguglani0897@gmail.com>
2020-03-20 14:10:42 +0530

    function for displaying items complete

--- Shopping List App/shopping.js
+++ Shopping List App/shopping.js
@@ -1,3 +1,42 @@
 const form = document.querySelector(`.shopping-form`);
 const list = document.querySelector(`.shopping-list`);
-console.log(form, list);
+let items = [];
+
+//-----------FUNCTION FOR SUBMITTING FORM------------
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const name = e.currentTarget.item.value;
+  const item = {
+    name: name,
+    id: Date.now(),
+    completed: false
+  };
+  e.currentTarget.reset();
+  items.push(item);
+  list.dispatchEvent(new CustomEvent(`updateList`));
+}
+
+//----------FUNCTION FOR DISPLAYING LIST----------
+
+function displayList() {
+  const newitems = items.map(item => {
+    return `<li>
+        <input type = "checkbox" ></input>
+        ${item.name}
+        <button aria-label = "Remove ${item.name}"type = "submit">&times;</button>
+        </li>`;
+  });
+  list.innerHTML = newitems.join(``);
+}
+
+//function for displaying items
+//function for displaying
+//function for reloading
+//function for updation of list
+//function for deleting stuff
+//function for checkboxes
+
+form.addEventListener(`submit`, handleSubmit);
+
+list.addEventListener(`updateList`, displayList);
