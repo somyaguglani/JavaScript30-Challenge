a4cb32dfdf5ae89abd542aa864c6fe8bb16cc65c
Somya Guglani <somyaguglani0897@gmail.com>
2020-03-20 22:20:15 +0530

    js part complete of shopping list

--- Shopping List App/shopping.js
+++ Shopping List App/shopping.js
@@ -1,42 +1,104 @@
-const form = document.querySelector(`.shopping-form`);
-const list = document.querySelector(`.shopping-list`);
+const shoppingForm = document.querySelector(".shopping-form");
+const list = document.querySelector(".shopping-list");
+
+// We need an array to hold our state
 let items = [];
 
-//-----------FUNCTION FOR SUBMITTING FORM------------
+//--------FUNCTION FOR HANDLING SUBMIT------------
 
 function handleSubmit(e) {
   e.preventDefault();
   const name = e.currentTarget.item.value;
+  // if its empty, then dont submit it
+  if (!name) return;
+
   const item = {
-    name: name,
+    name,
     id: Date.now(),
-    completed: false
+    complete: false
   };
-  e.currentTarget.reset();
+  // Push the items into our state
   items.push(item);
-  list.dispatchEvent(new CustomEvent(`updateList`));
+  console.log(`There are now ${items.length} in your state`);
+  // Clear the form
+  e.target.reset();
+  // fire off a custom event that will tell anyone else who cares that the items have been updated!
+  list.dispatchEvent(new CustomEvent("itemsUpdated"));
 }
 
-//----------FUNCTION FOR DISPLAYING LIST----------
+//----------FUNCTION FOR DISPLAYING LIST------------
 
-function displayList() {
-  const newitems = items.map(item => {
-    return `<li>
-        <input type = "checkbox" ></input>
-        ${item.name}
-        <button aria-label = "Remove ${item.name}"type = "submit">&times;</button>
-        </li>`;
-  });
-  list.innerHTML = newitems.join(``);
+function displayItems() {
+  const html = items
+    .map(
+      item => `<li >
+      <input
+        value="${item.id}"
+        type="checkbox"
+        ${item.complete && "checked"}
+      >
+      <span class="itemName">${item.name}</span>
+      <button
+        aria-label="Remove ${item.name}"
+        value="${item.id}"
+      >&times;</buttonaria-label="Remove>
+  </li>`
+    )
+    .join("");
+  list.innerHTML = html;
 }
 
-//function for displaying items
-//function for displaying
-//function for reloading
-//function for updation of list
-//function for deleting stuff
-//function for checkboxes
+//----------FUNCTION FOR LOCAL STORAGE---------------
 
-form.addEventListener(`submit`, handleSubmit);
+function mirrorToLocalStorage() {
+  localStorage.setItem("items", JSON.stringify(items));
+}
 
-list.addEventListener(`updateList`, displayList);
+//----------FUNCTION FOR RESTORING FROM STORAGE------------
+
+function restoreFromLocalStorage() {
+  // pull the items from LS
+  const lsItems = JSON.parse(localStorage.getItem("items"));
+  if (lsItems.length) {
+    // items = lsItems;
+    // lsItems.forEach(item => items.push(item));
+    // items.push(lsItems[0], lsItems[1]);
+    items.push(...lsItems);
+    list.dispatchEvent(new CustomEvent("itemsUpdated"));
+  }
+}
+
+//-------------FUNCTIONS FOR EVENT DELEGATION--------------
+
+//-----------FUNCTION FOR DELETING ITEMS--------------
+
+function deleteItem(id) {
+  // update our items array without this one
+  items = items.filter(item => item.id !== id);
+  list.dispatchEvent(new CustomEvent("itemsUpdated"));
+}
+
+//----------FUNCTION FOR CHECKBOX--------------
+
+function markAsComplete(id) {
+  const itemRef = items.find(item => item.id === id);
+  itemRef.complete = !itemRef.complete;
+  list.dispatchEvent(new CustomEvent("itemsUpdated"));
+}
+
+//------------EVENT LISTNERS-------------------
+
+shoppingForm.addEventListener("submit", handleSubmit);
+list.addEventListener("itemsUpdated", displayItems);
+list.addEventListener("itemsUpdated", mirrorToLocalStorage);
+// Event Delegation: We listen or the click on the list <ul> but then delegate the click over to the button if that is what was clicked
+list.addEventListener("click", function(e) {
+  const id = parseInt(e.target.value);
+  if (e.target.matches("button")) {
+    deleteItem(id);
+  }
+  if (e.target.matches('input[type="checkbox"]')) {
+    markAsComplete(id);
+  }
+});
+restoreFromLocalStorage();
