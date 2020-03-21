560e9cd5b8f493e60067259af66e0ebe789aa4e3
Somya Guglani <somyaguglani0897@gmail.com>
2020-03-22 05:23:10 +0530

    half of js complete for gallery

--- Building a Gallery/script.js
+++ Building a Gallery/script.js
@@ -1,15 +1,38 @@
 function Gallery(gallery) {
-  if (!gallery) return;
+  if (!gallery) throw new Error(`gallery not found`);
 
   const imagesArray = gallery.querySelectorAll(`img`);
   const modal = document.querySelector(`.modal`);
   const prevButton = modal.querySelector(`.prev`);
   const nextButton = modal.querySelector(`.next`);
+  let currentImage;
+  function openModal() {
+    modal.classList.add(`open`);
+    prevButton.addEventListener(`click`, showPrevImage);
+    nextButton.addEventListener(`click`, showNextImage);
+  }
 
+  function closeModal() {
+    //cmplete this
+    modal.classList.remove(`open`);
+  }
+  //handle keys
+
+  function showPrevImage() {
+    showPhoto(currentImage.previousElementSibling || gallery.lastElementChild);
+  }
+
+  function showNextImage() {
+    showPhoto(currentImage.nextElementSibling || gallery.firstElementChild);
+  }
   function showPhoto(e) {
     if (!e) return;
-    console.dir(e.src, e.title, e.dataset.description);
+    currentImage = e;
+    console.log(e);
     modal.querySelector(`img`).src = e.src;
+    modal.querySelector(`h2`).src = e.title;
+    modal.querySelector(`p`).src = e.dataset.description;
+    openModal();
   }
 
   imagesArray.forEach(image => {
