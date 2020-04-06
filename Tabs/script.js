81e75d3e215f599323fd6d798a6ede1f6b73d066
Somya Guglani <somyaguglani0897@gmail.com>
2020-04-06 22:38:26 +0530

    js of tabs complete halfway

--- Tabs/script.js
+++ Tabs/script.js
@@ -1,1 +1,18 @@
+const tabContainer = document.querySelector(`.tabContainer`);
+const tabButtons = tabContainer.querySelectorAll(`[role = "tab"]`);
+console.log(tabButtons);
+const tabPanels = tabContainer.querySelectorAll(`[role = "tabpanel"]`);
 
+const handleClick = (e) => {
+  //remove selection from everything else
+  tabPanels.forEach((tabpanel) => (tabpanel.hidden = true));
+  tabButtons.forEach((button) => button.setAttribute(`area-selected`, false));
+  tabPanels.forEach((tabpanel) => {
+    if (tabpanel.ariaLabelledby === e.currentTarget.id) {
+      console.log(e.currentTarget.id);
+      tabpanel.hidden = false;
+    }
+  });
+};
+
+tabButtons.forEach((button) => button.addEventListener(`click`, handleClick));
