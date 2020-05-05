8884e5978fbd2765d07a2b94b4e5b6cc642f14f7
Somya Guglani <somyaguglani0897@gmail.com>
2020-05-05 22:14:41 +0530

    errors in promises

--- Promises/script.js
+++ Promises/script.js
@@ -1,7 +1,9 @@
 //basics of promises
 
 function Pizza(toppings) {
+  //reject if toppings include pinapple pizza
   const pizzaPromise = new Promise(function (resolved, rejected) {
+    if (toppings.includes(`pineapple`)) rejected(`seriously? get out `);
     setTimeout(function () {
       resolved(`here is your baked pizza - ${toppings.join(` `)}`);
     }, 2000);
@@ -9,29 +11,47 @@ function Pizza(toppings) {
   return pizzaPromise;
 }
 
-const pizzaPromise1 = new Pizza([`olives`, `extra cheese`, `juicy chicken`]);
-const pizzaPromise2 = new Pizza([`tomato`, `extra cheese`, `corn`]);
-const pizzaPromise3 = new Pizza([`tomato`, `extra cheese`]);
+const pizzaPromise1 = Pizza([`olives`, `extra cheese`]);
+// pizzaPromise1
+//   .then(function (message) {
+//     console.log(message);
+//   })
+//   .catch(function (message) {
+//     console.log(`you got rejected`);
+//     console.log(message);
+//   });
+
+const pizzaPromise2 = Pizza([`tomato`, `extra cheese`, `corn`, `pineapple`]);
+const pizzaPromise3 = Pizza([`tomato`, `extra cheese`]);
 // console.log(pizzaPromise1);
 // pizzaPromise1
 //   .then(function (message) {
 //     console.log(message);
-//     return new Pizza([`tomato`, `extra cheese`, `corn`]);
+//     return Pizza([`tomato`, `extra cheese`]);
 //   })
 //   .then(function (message) {
 //     console.log(message);
-//     return new Pizza([`tomato`, `extra cheese`, `corn`, `last`]);
+//     return Pizza([`tomato`, `extra cheese`, `corn`, `last`, `pineapple`]);
+//   })
+//   .catch(function (message) {
+//     console.log(`you got rejected`);
+//     console.log(message);
 //   });
-
-const dinnerPromise = Promise.all([
+const dinnerPromise = Promise.allSettle([
   pizzaPromise1,
-  pizzaPromise2,
+  pizzaPromise1,
   pizzaPromise3,
-]).then(function (pizza) {
-  console.log(pizza);
-});
+]).then((things) => console.log(things));
 
-const firstPizza = Promise.race([pizzaPromise1, pizzaPromise2, pizzaPromise3]);
-firstPizza.then(function (pizza) {
-  console.log(`you got the first pizza ${pizza}`);
-});
+// const dinnerPromise = Promise.all([
+//   pizzaPromise1,
+//   pizzaPromise2,
+//   pizzaPromise3,
+// ]).then(function (pizza) {
+//   console.log(pizza);
+// });
+
+// const firstPizza = Promise.race([pizzaPromise1, pizzaPromise2, pizzaPromise3]);
+// firstPizza.then(function (pizza) {
+//   console.log(`you got the first pizza ${pizza}`);
+// });
